d09e1ca227de40034ae790d9aab218a3b007f294
indira-faizullina <indiramus@list.ru>
2025-10-25 11:17:01 +0300

    upload files with multer

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
-import 'dotenv/config'
+import "dotenv/config"
 import { registerValidation, loginValidation, postCreateValidation } from "./validations.js"
 import checkAuth from "./utils/checkAuth.js"
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
+import * as UserController from "./controllers/UserController.js"
+import * as PostController from "./controllers/PostController.js"
+import multer from "multer"
 
 const DB_URL = process.env.DB_URL
 
@@ -13,20 +14,35 @@ mongoose.connect(DB_URL)
 .catch((err) => console.log('Error', err))
 
 const app = express()
-
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'uploads')
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname)
+    }
+})
+
+const upload = multer({storage})
 
 app.post('/auth/login', loginValidation, UserController.login)
-
 app.post('/auth/register', registerValidation, UserController.register)
-
 app.get('/auth/me', checkAuth, UserController.authMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.status(200).json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
 
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, PostController.update)
 
 app.listen(4000, (err) => {
     if(err) {
